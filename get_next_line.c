5e787ddd473dc73dbacbff8d63d3481a6927bd92
Melanie Ferraz Reis <meferraz@c1r2s5.42porto.com>
2024-11-07 16:14:21 +0000

    add all files

---
+++ get_next_line.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meferraz <meferraz@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 13:28:27 by meferraz          #+#    #+#             */
+/*   Updated: 2024/11/07 13:28:45 by meferraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./get_next_line.h"
+#include <unistd.h>
+
+static char	*read_and_store(int fd, char *buff);
+char	*extract_line(char *buff);
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char *line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = read_and_store(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = extract_line(buffer);
+	buffer = update_buffer(buffer);
+	return (line);
+}
+
+static char	*read_and_store(int fd, char *buff)
+{
+	char	*buffer;
+	ssize_t	bytes_read;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr(buff, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free (buffer);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		buff = ft_strjoin(buff, buffer);
+	}
+	free(buffer);
+	return (buff);
+}
+
+char	*extract_line(char *buff)
+{
+	size_t	line_size;
+	size_t	i;
+	char	*line;
+
+	line_size = 0;
+	if (!buff)
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		line_size++;
+	line = malloc(sizeof(char) * (i + 2)); // +1 for '\n', +1 for '\0'
+	if (!line)
+		return (NULL);
+	i = -1;
+	while (++i < line_size + 1)
+		line[i] = buff[i];
+	line[i] = '\0';
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meferraz <meferraz@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 13:28:55 by meferraz          #+#    #+#             */
+/*   Updated: 2024/11/07 13:30:13 by meferraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
+
+char	*get_next_line(int fd);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
